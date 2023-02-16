4068d518084345574b9c5f846ef356f1d60df613
marsa <marsa@EVERGLOW>
2023-02-16 21:47:03 +0200

    Add project files.

---
+++ testtask/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testtask
+{
+    public partial class Form1 : Form
+    {
+        public static string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            path = textBox1.Text;
+            
+        }
+    }
+}
---
+++ testtask/istream.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtask
+{
+    internal class istream
+    {
+        Form1 form1 = new Form1();
+        string path = Form1.path;
+
+    }
+}
