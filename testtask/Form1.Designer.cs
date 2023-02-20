bfbbbd4bd312a4ef884c5865f6e98d3fdbc3d75e
marsa <marsa@EVERGLOW>
2023-02-20 11:41:55 +0200

    stage1

--- testtask/Form1.Designer.cs
+++ testtask/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(336, 188);
+            this.button1.Location = new System.Drawing.Point(1080, 85);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 36);
+            this.button1.Size = new System.Drawing.Size(280, 53);
             this.button1.TabIndex = 0;
             this.button1.Text = "sumbit";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,29 +48,51 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(265, 123);
+            this.label1.Location = new System.Drawing.Point(1052, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 25);
+            this.label1.Size = new System.Drawing.Size(377, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "submit path of input file";
+            this.label1.Text = "submit  path of input file and threshold";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(251, 151);
+            this.textBox1.Location = new System.Drawing.Point(915, 48);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(266, 31);
+            this.textBox1.Size = new System.Drawing.Size(323, 31);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(553, 144);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1328, 532);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1277, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2534, 1369);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -80,6 +104,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- testtask/Form1.cs
+++ testtask/Form1.cs
@@ -5,9 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Markup;
+using static System.Windows.Forms.LinkLabel;
 
 namespace testtask
 {
@@ -26,8 +29,92 @@ namespace testtask
 
         private void button1_Click(object sender, EventArgs e)
         {
-            path = textBox1.Text;
             
+                //int threshold = Int32.Parse(textBox2.Text);
+                path = @"C:\users\marsa\c.csv";
+                List<string> lines = new List<string>();
+                List<int> ints = new List<int>();
+                List<int> supersorted = new List<int>();
+                List<int> hth= new List<int>();
+
+
+            using (var reader = new StreamReader(path))
+            {
+
+                while (!reader.EndOfStream)
+                {
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    foreach (var item in values)
+                    {
+                        lines.Add(item);
+                    }
+
+                }
+
+            }
+            int rows = int.Parse(lines[0]);
+            int columns = int.Parse(lines[1]);
+            foreach (var item in lines)
+            {
+                int.TryParse(item, out int num);
+                ints.Add(num);
+            }
+
+
+            for (int r = rows*columns; r < ints.Count; r++)
+            {
+                supersorted.Add(ints[r]);
+
+            }
+            int frames = supersorted.Count / (rows * columns);
+            //int count = 0;
+            //do
+            //{
+            //    for (int j = 0; j < columns; j++)
+            //    {
+            //        richTextBox1.Text += supersorted[count].ToString() + "      ";
+            //        count++;
+
+            //    }
+            //    richTextBox1.Text += "\n";
+            //}
+            //while (count < supersorted.Count);
+            richTextBox1.Text += supersorted.Count.ToString() + " " + frames.ToString();
+            
+
+            //foreach (int num in hth)
+            //{
+            //    richTextBox1.Text += num.ToString();
+            //}
+            //richTextBox1.Text = lines.Count.ToString();
+
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
